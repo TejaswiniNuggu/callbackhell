77f0ac4882f34fe522fbf0ab20bd360d8ffcc7c0
TejaswiniNuggu <99638695+TejaswiniNuggu@users.noreply.github.com>
2022-03-29 12:51:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+setTimeout(function(){ 
+    let count=10;
+    for(let i=count; i>0; i--)
+    {
+    let value=document.getElementById("clock")
+    value=document.write(i+"\n")
+    if(i==1){
+    document.write("Happy Independence Day"); }}
+    }, 1000);
+
